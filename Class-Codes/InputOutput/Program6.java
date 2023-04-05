5229a70785b3f9d504fc920dc36d6c7e5adae983
Sayali-Lohokare <sayalisunillohokare@gmail.com>
2023-04-05 21:18:20 +0530

    Class-code added

---
+++ Class-Codes/InputOutput/Program6.java
@@ -0,0 +1,15 @@
+import java.io.*;
+class Demo{
+	public static void main(String[] args)throws IOException{
+		BufferedReader br1 = new BufferedReader(InputStreamReader(System.in));
+		BufferedReader br2 = new BufferedReader(InputStreamReader(System.in));
+
+		String str1=br1.readLine();
+		br1 = close();
+
+		String str2=br2.readLine();
+	}
+}
+
+
+
