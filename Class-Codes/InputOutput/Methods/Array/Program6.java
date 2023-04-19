0a0c03a8a613269ea67ed95d69765d19e4700de3
Sayali-Lohokare <sayalisunillohokare@gmail.com>
2023-04-19 11:08:58 +0530

    Class-codes added

---
+++ Class-Codes/InputOutput/Methods/Array/Program6.java
@@ -0,0 +1,10 @@
+class Demo{
+	public static void main(String[] args){
+		float arr1[] = new float[4];
+		System.out.println(arr1[0]);
+		System.out.println(arr1[1]);
+		System.out.println(arr1[2]);
+		System.out.println(arr1[3]);
+	}
+}
+
