7dadff6aa6f15fa62c26f68c062a43106d39ad46
Sayali-Lohokare <sayalisunillohokare@gmail.com>
2023-04-28 00:01:12 +0530

    Class-codes added

---
+++ Class-Codes/Array/Passingarrayasanargument/Program3.java
@@ -0,0 +1,10 @@
+class Demo{
+	static int fun(int x){
+		return x;
+	}
+	public static void main(String[] args){
+		int x = 10;
+		System.out.println(x);
+	}
+}
+
---
+++ Class-Codes/Array/Passingarrayasanargument/Program6.java
@@ -0,0 +1,13 @@
+class Demo{
+	static void fun(int xarr[]){
+		fun(int x : xarr){
+			System.out.println(x);
+		}
+	}
+	public static void main(String[] args){
+		int arr[] = {10,20,30,40};
+		fpr(int x : arr){
+			System.out.println(x);
+		}
+	}
+}
