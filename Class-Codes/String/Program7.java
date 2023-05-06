3c92eaef2befcda91959ebf66b439f965080951b
Sayali-Lohokare <sayalisunillohokare@gmail.com>
2023-05-06 16:15:24 +0530

    Class-codes added

---
+++ Class-Codes/String/Program7.java
@@ -0,0 +1,9 @@
+class EqualsIgnoreDemo{
+	public static void main(String[] args){
+
+		String str1 = "SHASHI";
+		String str2 = "shashi";
+
+		System.out.println(str1.equalsIgnoreCase(str2));
+	}
+}
