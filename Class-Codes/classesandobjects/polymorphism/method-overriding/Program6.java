01d5d8adf39f6b138bd01788b9a888062424eae0
Sayali-Lohokare <sayalisunillohokare@gmail.com>
2023-06-13 20:02:04 +0530

    Class-codes added

--- Class-Codes/classesandobjects/polymorphism/method-overriding/Program6.java
+++ Class-Codes/classesandobjects/polymorphism/method-overriding/Program6.java
@@ -1,23 +1,24 @@
 class Parent {
     Parent(){
-        System.out.println("in parent constructor");
+        System.out.println("in parent construtor");
     }
-    void fun(){
+    void fun(int x){
         System.out.println("in parent fun");
     }
 }
 class Child extends Parent{
     Child(){
-        System.out.println("child constructor");
+        System.out.println("in child constructor");
     }
-    void fun(int x){
-        System.out.println("in child fun");
+    void fun(){
+        System.out.println("in child constructor");
     }
-
 }
 class Client{
     public static void main(String[] args){
         Parent obj = new Child();
         obj.fun();
+
     }
+    
 }
