b7e56efe12d8ce7abd912b2ff6f418887abee051
Sayali-Lohokare <sayalisunillohokare@gmail.com>
2023-07-11 22:56:41 +0530

    Class-codes added

---
+++ Class-Codes/classesandobjects/inheritance/Program7.java
@@ -0,0 +1,10 @@
+//Real time example
+class OperatingSystem {
+    OperatingSystem(){
+        System.out.println("In OS Constructor");
+    }
+    void Networking{
+        System.out.println("Qual")
+    }
+    
+}
