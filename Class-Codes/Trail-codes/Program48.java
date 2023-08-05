dc3f6766d12395b21417c6e1eccaee9a83a6c082
Sayali-Lohokare <sayalisunillohokare@gmail.com>
2023-08-05 22:14:17 +0530

    Class-codes added

---
+++ Class-Codes/Trail-codes/Program48.java
@@ -0,0 +1,23 @@
+class Demo{
+
+	int fun(int a, int b){
+
+	int c =2;
+	b = b%c;
+	a = a%c;
+
+	return (a+b);
+	}
+
+
+	public static void main(String[] args){
+
+		int a = 9;
+		int b = 7;
+
+		Demo obj = new Demo();
+		obj.fun(a,b);
+
+		System.out.println(a+b);
+	}
+}
