021470041e18927226507c34c2002609c144a401
Sayali-Lohokare <124552316+Sayali-Lohokare@users.noreply.github.com>
2023-08-18 22:50:43 +0530

    Update Program1.java

--- Assignment/String/Stringbasic/Program1.java
+++ Assignment/String/Stringbasic/Program1.java
@@ -14,6 +14,6 @@ class LargestCharDemo{
 				largestchar=ch;
 			}
 		}
-		System.out.println("Largest character =" + largestchar);
+		//System.out.println("Largest character =" + largestchar);
 	}
 }
