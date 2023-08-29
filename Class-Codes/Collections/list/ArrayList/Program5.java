c3b606a47ef44e9454b83294f4c7ad63861ee824
Sayali-Lohokare <sayalisunillohokare@gmail.com>
2023-08-29 22:33:45 +0530

    Class-codes added

--- Class-Codes/Collections/list/ArrayList/Program5.java
+++ Class-Codes/Collections/list/ArrayList/Program5.java
@@ -1,8 +1,15 @@
-import java.util.*;
+/*import java.util.*;
 class Demo{
 	public static void main(String[] args){
 
 		ArrayList o = new arrayList();
 		List l1 = collections.synchronizedList(a);
+	}
+}*/
+
+class Demo{
+	public static void main(String[] args){
+		if(System.out.printf("hello world")==null){
+		}
 	}
 }
