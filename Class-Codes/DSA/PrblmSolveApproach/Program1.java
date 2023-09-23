bd846a8fc9464c74da42aebccaac710a36cb57e9
Sayali-Lohokare <sayalisunillohokare@gmail.com>
2023-09-23 23:28:11 +0530

    Class-codes added

---
+++ Class-Codes/DSA/PrblmSolveApproach/Program1.java
@@ -0,0 +1,26 @@
+import java.io.*;
+class SquareRoot{
+
+	static int find(int num){
+
+	for(int i=1;i<=num;i++){
+		if(i*i<=num){
+		
+	}
+	return num;
+	}
+	}
+
+	public static void main(String[] args)throws IOException{
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int num = Integer.parseInt(br.readLine());
+		System.out.println(num);
+		//int num = br.readLine();
+
+	//	return num;
+	}
+}
+
+
+
