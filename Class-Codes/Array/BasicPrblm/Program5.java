d003cde53a42ad7082258a575e0faf57eee092c3
Sayali-Lohokare <sayalisunillohokare@gmail.com>
2023-10-02 23:55:09 +0530

    Class-codes added

--- Class-Codes/Array/BasicPrblm/Program5.java
+++ Class-Codes/Array/BasicPrblm/Program5.java
@@ -1,4 +1,4 @@
-import java.io.*;
+/*import java.io.*;
 class ReplaceZero{
 	public static void main(String[] args)throws IOException{
 
@@ -27,4 +27,92 @@ class ReplaceZero{
 
 		//System.out.println(output);
 	}
+}*/
+
+
+
+/*import java.io.*;
+class ReplaceDemo{
+
+	static int find(int arr[]){
+
+		int val = -1;
+
+		for(int i=0;i<arr.length;i++){
+			if(arr[i]==0){
+				arr[i]=5;
+			
+			}
+			else{
+				val = arr[i];
+			}
+			//return arr[i];
+		}
+		return val;
+	}
+	public static void main(String[] args)throws IOException{
+
+		BufferedReader br  = new BufferedReader(new InputStreamReader(System.in));
+
+		System.out.println("enter the number");
+		int n = Integer.parseInt(br.readLine());
+
+		int arr[] = new int[n];
+
+		for(int i=0;i<arr.length;i++){
+			arr[i]=Integer.parseInt(br.readLine());
+		}
+
+		 find(arr);
+
+		 
+
+		for(int i=0;i<arr.length;i++){
+
+
+		System.out.println(arr[i]);
+		}
+	}
+}*/
+
+
+
+
+import java.io.*;
+class ReplaceDemo{
+	 
+	static int ArrayList find(int ArrayList){
+
+		for(int i=0;i<arr.length;i++){
+			if(list.get(i)==0){
+				list.set(i,5);
+			}
+		}
+		return list;
+	}
+	public static void main(String[] args)throws IOException{
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		System.out.println("enter the number");
+		int n = Integer.parseInt(br.readLine());
+
+		ArrayList al = new ArrayList();
+
+		System.out.println("enter the elements");
+		for(int i=0;i<arr.length;i++){
+			list.add(Integer.parseInt(br.readLine()));
+		}
+
+		int output = find(int ArrayList);
+
+		for(int i=0;i<arr.length;i++){
+			System.out.println(output);
+		}
+	}
 }
+
+
+
+
+
