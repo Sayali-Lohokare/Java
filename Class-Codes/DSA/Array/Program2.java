4df48bd8a1f7e3ca74a715b8ff0757a11afdbc4f
Sayali-Lohokare <sayalisunillohokare@gmail.com>
2023-10-04 23:30:34 +0530

    Class-codes added

--- Class-Codes/DSA/Array/Program2.java
+++ Class-Codes/DSA/Array/Program2.java
@@ -1,7 +1,7 @@
 //count no of elements having atleast 1 element greater than itself
 
 
-class Element{
+/*class Element{
 	public static void main(String[] args){
 
 		int n = 10;
@@ -25,17 +25,17 @@ class Element{
 		}
 		System.out.println(count);
 	}
-}
+}*/
 
 
 
-/*class Element{
+class Element{
 	public static void main(String[] args){
 
 		int n = 10;
 		int arr[] = new int[]{2,5,1,4,8,0,8,1,3,8};
 		int count=0;
-		int max = Integer.MIN_VALUE;
+		int maxi = Integer.MIN_VALUE;
 
 		for(int i=0;i<n;i++){
 			if(arr[i]>max){
@@ -49,5 +49,5 @@ class Element{
 			System.out.println(n-count);
 		}
 	}
-}*/
+}
 
