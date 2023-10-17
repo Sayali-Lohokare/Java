0024ff4db430d9808f9c432be4f8df3fd86779fe
Sayali-Lohokare <sayalisunillohokare@gmail.com>
2023-10-17 23:56:54 +0530

    Class-codes added

---
+++ Class-Codes/DSA/Array/Program5.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Demo{
+	public static void main(String[] arhs){
+		HashSet hs = new HashSet();
+		System.out.println(hs.add(5));
+		System.out.println(hs.add(4));
+		System.out.println(hs.add(11));
+		System.out.println(hs);
+
+	}
+}
+
